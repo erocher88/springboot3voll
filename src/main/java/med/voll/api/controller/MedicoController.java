239e450b794fe0834b737e11c57a3c497bba890e
erocher88 <erocher88@outlook.com>
2023-05-20 00:43:50 -0600

    Added GET for list using Pageables, set order and size of list, modified properties of Console message about queries

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,12 +1,17 @@
 package med.voll.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
+import med.voll.api.medico.DatosListadoMedicos;
 import med.voll.api.medico.DatosRegistroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
@@ -23,6 +28,11 @@ public class MedicoController {
 
 		medicoRepository.save(new Medico(datosRegistroMedico));
 		
+	}
+	
+	@GetMapping
+	public Page<DatosListadoMedicos> listadoMedicos(@PageableDefault(size = 2) Pageable paginacion) {
+		return medicoRepository.findAll(paginacion).map(DatosListadoMedicos::new);
 	}
 	
 }
