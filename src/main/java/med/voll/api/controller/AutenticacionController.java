2cddbaa676a247cb584068296ab7fc882df3c387
erocher88 <erocher88@outlook.com>
2023-05-22 23:24:35 -0600

    Added token to authorize session

--- src/main/java/med/voll/api/controller/AutenticacionController.java
+++ src/main/java/med/voll/api/controller/AutenticacionController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import med.voll.api.domain.usuarios.DatosAutenticacionUsuario;
+import med.voll.api.domain.usuarios.Usuario;
+import med.voll.api.infra.security.DatosJWTToken;
+import med.voll.api.infra.security.TokenService;
 
 @RestController
 @RequestMapping(value="/login")
@@ -20,14 +23,17 @@ public class AutenticacionController {
 	@Autowired
 	private AuthenticationManager authenticationManager;
 	
+	@Autowired
+	private TokenService tokenService;
+	
 	@PostMapping
 	public ResponseEntity autenticarUsuario(@RequestBody @Valid DatosAutenticacionUsuario datosAutenticacionUsuario) {
 		
-		Authentication token = new UsernamePasswordAuthenticationToken(datosAutenticacionUsuario.login(), 
+		Authentication authToken = new UsernamePasswordAuthenticationToken(datosAutenticacionUsuario.login(), 
 				datosAutenticacionUsuario.clave());
-		authenticationManager.authenticate(token );
-		
-		return ResponseEntity.ok().build();
+		var usuarioAutenticado = authenticationManager.authenticate(authToken);
+		var jwtToken = tokenService.generarToken((Usuario) usuarioAutenticado.getPrincipal());
+		return ResponseEntity.ok(new DatosJWTToken(jwtToken));
 		
 	}
 	
