8d8cbb9717ecef59042eba057bdad7821c5a8ec1
erocher88 <erocher88@outlook.com>
2023-05-24 01:33:36 -0600

    Added filters and auth methods to all except login

--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 import med.voll.api.domain.usuarios.Usuario;
 
@@ -35,8 +37,29 @@ public class TokenService {
 		
 	}
 	
+	public String getSubject(String token) {
+		if (token == null) {
+			throw new RuntimeException();
+		}
+		DecodedJWT verifier = null;
+		try {
+			Algorithm algorithm = Algorithm.HMAC256(apiSecret); // validando firma
+			verifier = JWT.require(algorithm)
+					.withIssuer("voll med")
+					.build()
+					.verify(token);
+			
+		} catch (JWTVerificationException exception) {
+			System.out.println(exception.toString());
+		}
+		if (verifier == null || verifier.getSubject() == null) {
+			throw new RuntimeException("Verifier invalido");
+		}
+		return verifier.getSubject();
+	}
+	
 	private Instant generarFechaExpiracion() {
-		return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-01:00"));
+		return LocalDateTime.now().plusHours(10).toInstant(ZoneOffset.of("-01:00"));
 	}
 	
 }
